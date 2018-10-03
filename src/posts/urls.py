404080deec1d8896f38766ec5317600b2ae13800
prolifixs <prolifixs.pj@gmail.com>
2018-10-03 14:45:36 +0300

    Post edit available. also redirects you after post edit is successful

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     url(r'^$', posts_list),
     url(r'^create/$', posts_create),#url(r'^$', "<appname>.view_module.<function_name>")
     url(r'^(?P<id>\d+)/$', posts_detail, name='detail'),
-    url(r'^update/$', posts_update),
+    url(r'^(?P<id>\d+)/edit/$', posts_update, name='update'),
     url(r'^delete/$', posts_delete),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from .models import Post
@@ -10,6 +10,7 @@ def posts_create(request): #As create
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())#Redirects page after post create
 	context = {
 		"form" : form,
 	}
@@ -32,8 +33,19 @@ def posts_list(request): #As retrieve
 	return render(request, "index.html", context)
 	#return HttpResponse("<h1>Lists</h1>")
 
-def posts_update(request): #As update
-	return HttpResponse("<h1>Update</h1>")
+def posts_update(request, id=None): #As update
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())#Redirects page after post update
+	context = {
+	"title" : instance.title,
+	"form" : form,
+	"instance": instance
+	}
+	return render(request, "posts_form.html", context)
 
 def posts_delete(request): #As delete
 	return HttpResponse("<h1>Delete</h1>")
