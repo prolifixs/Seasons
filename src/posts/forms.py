ead39420c176cea9746f8af91a5cadd8045a75e2
prolifixs <prolifixs.pj@gmail.com>
2018-10-03 13:51:22 +0300

    created a model form to handle create. Also, template for showing the form and its contents

---
+++ src/posts/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):#To be used in create viev
+	class Meta:	
+		model = Post
+		fields = [
+			"title",
+			"context"
+		]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 from .models import Post
+
 # Create your views here.  C R U D - Create, retrieve, update, delete
 
 def posts_create(request): #As create
-	return HttpResponse("<h1>Create</h1>")
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+	context = {
+		"form" : form,
+	}
+	return render(request, "posts_form.html", context)
 
 def posts_detail(request, id): #As retrieve
 	instance = get_object_or_404(Post, id=id)
