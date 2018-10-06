31b480d0d4283017ce1ea6335513b9425f4e7326
prolifixs <prolifixs.pj@gmail.com>
2018-10-06 17:30:35 +0300

    ----DEPRICATED--- Slug is not working. reverting back to using post id till probelm is resolved. to change back to slug, in models.py at methos get_absolut_url. kwargs is commented out

--- src/posts/models.py
+++ src/posts/models.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 
 def upload_location(instance, filename):#giving images unique paths using id
@@ -9,7 +11,8 @@ def upload_location(instance, filename):#giving images unique paths using id
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=120)
-	image = models.ImageField(upload_to = upload_location ,null=True, blank=True, 
+	slug = models.SlugField(unique=True)
+	image = models.ImageField(upload_to = upload_location, null=True, blank=True, 
 		width_field= 'width_field', height_field= 'height_field')
 	height_field = models.IntegerField(default=0)
 	width_field = models.IntegerField(default=0)
@@ -23,7 +26,25 @@ class Post(models.Model):
 		return self.title
 
 	def get_absolute_url(self):
-		return reverse("posts:detail", kwargs={"id":self.id})
+		return reverse("posts:detail", kwargs={"id": self.id})#kwargs={"slug": self.slug})
 		#return "%s/" %(self.id) #return "posts/%s/" %(self.id)
 	class Meta:
 		ordering = ["-timestamp", "-updated"]
+
+
+def create_slug(instance, new_slug=None):
+	slug = slugify(instance.title)
+	if new_slug is not None:
+		slug = new_slug	
+	qs = Post.objects.filter(slug=slug).order_by("-id")
+	exists = qs.exists()
+	if exists:
+		new_slug = "%s-%s" %(slug, qs.first().id)
+		return create_slug(instance, new_slug=new_slug)
+	return slug
+
+def pre_save_post_reciever(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = create_slug(instance) 	
+
+pre_save.connect(pre_save_post_reciever, sender=Post)
--- src/seasons/urls.py
+++ src/seasons/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf import settings
+from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.conf.urls import include, url #"include" to use custom urls from apps
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls", namespace='posts')),#url(r'^$', "posts.views.posts_home") //formal global url
+    url(r'^posts/', include("posts.urls", namespace='posts')),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
